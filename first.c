b43e6ea6b1bcf149cb79684de110a95ca93cc237
Manish Prajapati <mp662388@gmail.com>
2026-08-29 22:33:42 +0530

    Integer Literal

--- first.c
+++ first.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main() 
 {
-    char name [5] = "Manish";
-    //print hello "Manish" how are you
-    printf("Hello %s, how are you?\n", name);
+    int i = 012;
+    int j = 0b100;
+    int k = 0X100;
+    printf("integer value is = %i\n", i);
+    printf("integer value is = %i\n", j);
+    printf("integer value is = %i\n", k);
     return 0;
 }
  
