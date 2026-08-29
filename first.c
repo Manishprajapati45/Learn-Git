4dee8115ea33b2d4559574069d1138e4b7c08ead
Manish Prajapati <mp662388@gmail.com>
2026-08-29 19:36:15 +0530

    basics of string

--- first.c
+++ first.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main() 
 {
-    int age = 25;
-    float f = 8.9;
-    double d = 9.8;
-    char ch = 'A';
-    printf("age = %d\n", age);
-    printf("float = %.2f\n", f);
-    printf("double = %.2lf\n", d);
-    printf("char  = %c\n", ch); 
+    char name [5] = "Manish";
+    //print hello "Manish" how are you
+    printf("Hello %s, how are you?\n", name);
     return 0;
 }
+ 
