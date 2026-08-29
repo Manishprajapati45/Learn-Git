3d267edb6a8389f097625a7d2f5cbd160478adbd
Manish Prajapati <mp662388@gmail.com>
2026-08-29 19:17:17 +0530

    data type in c

--- first.c
+++ first.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main() 
 {
-    int abc = 19;
-    int ABC = 20;
-    printf("%i\n", abc);
-    printf("%i\n", ABC);
+    int age = 25;
+    float f = 8.9;
+    double d = 9.8;
+    char ch = 'A';
+    printf("age = %d\n", age);
+    printf("float = %.2f\n", f);
+    printf("double = %.2lf\n", d);
+    printf("char  = %c\n", ch); 
     return 0;
 }
