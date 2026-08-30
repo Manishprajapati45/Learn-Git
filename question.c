f2b78b887e1228ff96d602073c37380ff3b75e0f
Manish Prajapati <mp662388@gmail.com>
2026-08-30 11:29:20 +0530

    third question(calulate area of rectangle)

--- question.c
+++ question.c
@@ -1,14 +1,11 @@
-//Arithmetic operations
-// take two integers a & b with some constant values,Perform aritmetic operations (+,-,*,/,%). print with the appropriate message on the console.
+//Calculate Area of rectangle 
+// take length and breadth as input and print area.
 #include <stdio.h>
 int main()
 {
-    int a = 10;
+    int l = 10;
     int b = 7;
-    printf("The sum of %d and %d is: %d\n", a, b, a + b);
-    printf("The difference of %d and %d is: %d\n", a, b, a - b);
-    printf("The product of %d and %d is: %d\n", a, b, a * b);
-    printf("The quotient of %d and %d is: %d\n", a, b, a / b);
-    printf("The remainder of %d and %d is: %d\n", a, b, a % b);
+    int area = l*b;
+    printf("Area of rectangle is = %d\n", area);
     return 0;
 }
