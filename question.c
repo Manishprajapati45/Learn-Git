92c607b1d0a3a6f0ef0547e6d736659c3f204e02
Manish Prajapati <mp662388@gmail.com>
2026-08-30 11:22:51 +0530

    first question(sum of two integers)

---
+++ question.c
@@ -0,0 +1,11 @@
+//Sum of two integers
+//take two intergers a & b with some constant values, print their sum on the console.
+
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    return 0;
+}
