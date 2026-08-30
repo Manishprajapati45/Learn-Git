070dc7763a44c72cedb843984396c1b9aa69a3c6
Manish Prajapati <mp662388@gmail.com>
2026-08-30 11:25:23 +0530

    second question(arithmetic operations)

--- question.c
+++ question.c
@@ -1,11 +1,14 @@
-//Sum of two integers
-//take two intergers a & b with some constant values, print their sum on the console.
-
+//Arithmetic operations
+// take two integers a & b with some constant values,Perform aritmetic operations (+,-,*,/,%). print with the appropriate message on the console.
 #include <stdio.h>
-int main() {
-    int a = 5;
+int main()
+{
+    int a = 10;
     int b = 7;
-    int sum = a + b;
-    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    printf("The sum of %d and %d is: %d\n", a, b, a + b);
+    printf("The difference of %d and %d is: %d\n", a, b, a - b);
+    printf("The product of %d and %d is: %d\n", a, b, a * b);
+    printf("The quotient of %d and %d is: %d\n", a, b, a / b);
+    printf("The remainder of %d and %d is: %d\n", a, b, a % b);
     return 0;
 }
