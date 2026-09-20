7554c25e4cf4c13daceefa498b61e67d43278755
Manish Prajapati <147893395+Manishprajapati45@users.noreply.github.com>
2026-09-20 14:26:46 +0530

    variable question

--- question.c
+++ question.c
@@ -1,13 +1,20 @@
-//loop function code.
+//Take two variable [year and pi] year = 2023 and pi = 3.141590 print the given formate.
+
+
+
 
 #include <stdio.h>
+
 int main() {
-    int i;
+    // Declare and initialize your variables here
+    int year;
+    float pi;
+    year = 2023;
+    pi = 3.141590;
 
-    for(i = 1; i <= 100; i++) {
-        printf("%d\n", i);
+    // Your printf statements here
+    printf("The current year is %d\n", year);
+    printf("The value of pi is %f\n", pi);
 
-    }
     return 0;
-    
 }
