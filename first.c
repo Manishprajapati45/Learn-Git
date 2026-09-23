80ac3f687f8e5c28b4f4213d921927c6b3aa2bdc
Manish Prajapati <147893395+Manishprajapati45@users.noreply.github.com>
2026-09-24 00:44:32 +0530

    Arithmatic operaters

--- first.c
+++ first.c
@@ -1,11 +1,23 @@
+//Arithmatic operaters
+
 #include <stdio.h>
 
 int main() {
-    double temperature = 98.6;
-    int whole_part = (int) temperature;
-    
-    printf("Original temperature: %.1f\n", temperature);
-    printf("Whole part: %d\n", whole_part);
-    
+    // Declare and initialize variables here
+    int num1 = 15;
+    int num2 = 4;
+
+    // Perform calculations
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1 / num2;
+
+    // Print results
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+    printf("Product: %d\n", product);
+    printf("Quotient: %d\n", quotient);
+
     return 0;
 }
