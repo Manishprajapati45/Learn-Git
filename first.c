00054254640155f96d77ba3f8290d7028bb25c0d
Manish Prajapati <147893395+Manishprajapati45@users.noreply.github.com>
2026-09-24 21:21:19 +0530

    Increment/Decrement

--- first.c
+++ first.c
@@ -1,21 +1,18 @@
-//Modulo Operator
+//Increment/Decrement
+
+
 
 #include <stdio.h>
 
 int main() {
-    // Declare and initialize variables here
-    int number = 17;
+    // Write your code here
+    int number = 10;
+    int prefixResult = ++number;
+    int postfixResult = number++;
 
-    // Calculate remainder
-    int remainder = number % 5;
-
-    // Check if number is even or odd
-    int is_even = (number % 2 == 0);
-
-    // Print results
     printf("Number: %d\n", number);
-    printf("Remainder when divided by 5: %d\n", remainder);
-    printf("%d is even? %d\n", number, is_even);
+    printf("Prefix result: %d\n", prefixResult);
+    printf("Postfix result: %d\n", postfixResult);
     
     return 0;
 }
