74ac41fc882f6c0c34461fab087ee1db0db9eeed
Manish Prajapati <147893395+Manishprajapati45@users.noreply.github.com>
2026-09-25 14:30:56 +0530

    assigment operators

--- first.c
+++ first.c
@@ -1,18 +1,34 @@
-//Increment/Decrement
+//Assigment operators
 
+//. == simple operators
+//. += add and aasign
+//. -= subract and assign
+//. *= multiply and assign
+//. /= divide and assign
+//. %= modulo and assign
 
 
 #include <stdio.h>
 
-int main() {
-    // Write your code here
-    int number = 10;
-    int prefixResult = ++number;
-    int postfixResult = number++;
+#include <stdio.h>
 
-    printf("Number: %d\n", number);
-    printf("Prefix result: %d\n", prefixResult);
-    printf("Postfix result: %d\n", postfixResult);
+int main() {
+    int num = 10;
+    
+    num += 5;
+    printf("After += 5: %d\n", num);
+    
+    num -= 3;
+    printf("After -= 3: %d\n", num);
+    
+    num *= 2;
+    printf("After *= 2: %d\n", num);
+    
+    num /= 3;
+    printf("After /= 3: %d\n", num);
+    
+    num %= 4;
+    printf("After %%= 4: %d\n", num);
     
     return 0;
 }
